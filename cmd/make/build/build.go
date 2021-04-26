1c2efb9c3a29866e1f1199fc400e9fbbd2def59f
wuqinchao <wuqinchao@jiagouyun.com>
2021-04-26 14:55:27 +0800

    update

---
+++ cmd/make/build/build.go
@@ -0,0 +1,209 @@
+package build
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"text/template"
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/sec-checker/git"
+	"gitlab.jiagouyun.com/cloudcare-tools/sec-checker/logger"
+)
+
+var (
+	/* Use:
+		go tool dist list
+	to get current os/arch list */
+
+	OSArches = []string{ // supported os/arch list
+		`linux/386`,
+		`linux/amd64`,
+		`linux/arm`,
+		`linux/arm64`,
+	}
+)
+
+func runEnv(args, env []string) ([]byte, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+	if env != nil {
+		cmd.Env = append(os.Environ(), env...)
+	}
+
+	return cmd.CombinedOutput()
+}
+
+var (
+	l = logger.DefaultSLogger("build")
+
+	BuildDir     = "build"
+	PubDir       = "pub"
+	AppName      = "security-checker"
+	AppBin       = "checker"
+	OSSPath      = "security-checker"
+	Archs        string
+	Release      string
+	MainEntry    string
+	DownloadAddr string
+	ReleaseType  string
+)
+
+func prepare() *versionDesc {
+
+	os.RemoveAll(BuildDir)
+	_ = os.MkdirAll(BuildDir, os.ModePerm)
+	_ = os.MkdirAll(filepath.Join(PubDir, Release), os.ModePerm)
+
+	// create version info
+	vd := &versionDesc{
+		Version:  strings.TrimSpace(git.Version),
+		Date:     git.BuildAt,
+		Uploader: git.Uploader,
+		Branch:   git.Branch,
+		Commit:   git.Commit,
+		Go:       git.Golang,
+	}
+
+	versionInfo, err := json.MarshalIndent(vd, "", "    ")
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(PubDir, Release, "version"), versionInfo, 0666); err != nil {
+		l.Fatal(err)
+	}
+
+	return vd
+}
+
+func Compile() {
+	start := time.Now()
+
+	vd := prepare()
+
+	var archs []string
+
+	switch Archs {
+	case "all":
+		archs = OSArches
+
+		// read cmd-line env
+		if x := os.Getenv("ALL_ARCHS"); x != "" {
+			archs = strings.Split(x, "|")
+		}
+	case "local":
+		archs = []string{runtime.GOOS + "/" + runtime.GOARCH}
+		if x := os.Getenv("LOCAL"); x != "" {
+			archs = strings.Split(x, "|")
+		}
+	default:
+		archs = strings.Split(Archs, "|")
+	}
+
+	for idx := range archs {
+
+		parts := strings.Split(archs[idx], "/")
+		if len(parts) != 2 {
+			l.Fatalf("invalid arch %q", parts)
+		}
+
+		goos, goarch := parts[0], parts[1]
+		if goos == "darwin" && runtime.GOOS != "darwin" {
+			l.Warnf("skip build datakit under %s", archs[idx])
+			continue
+		}
+		dir := fmt.Sprintf("%s/%s-%s-%s", BuildDir, AppName, goos, goarch)
+
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			l.Fatalf("failed to mkdir: %v", err)
+		}
+
+		dir, err = filepath.Abs(dir)
+		if err != nil {
+			l.Fatal(err)
+		}
+
+		compileArch(AppBin, goos, goarch, dir, vd.Version)
+
+		if goos == "windows" {
+			installerExe = fmt.Sprintf("installer-%s-%s.exe", goos, goarch)
+		} else {
+			installerExe = fmt.Sprintf("installer-%s-%s", goos, goarch)
+		}
+
+	}
+
+	buildInstaller(strings.TrimSpace(git.Version), DownloadAddr)
+
+	l.Infof("Done!(elapsed %v)", time.Since(start))
+}
+
+func compileArch(bin, goos, goarch, dir, version string) {
+
+	output := filepath.Join(dir, bin)
+	if goos == "windows" {
+		output += ".exe"
+	}
+	cgo_enabled := "0"
+	if goos == "darwin" {
+		cgo_enabled = "1"
+	}
+
+	args := []string{
+		"go", "build",
+		"-o", output,
+		"-ldflags",
+		fmt.Sprintf("-w -s -X main.ReleaseType=%s -X main.Version=%s", ReleaseType, version),
+		MainEntry,
+	}
+
+	env := []string{
+		"GOOS=" + goos,
+		"GOARCH=" + goarch,
+		`GO111MODULE=off`,
+		"CGO_ENABLED=" + cgo_enabled,
+	}
+
+	l.Debugf("building %s", fmt.Sprintf("%s-%s/%s", goos, goarch, bin))
+	msg, err := runEnv(args, env)
+	if err != nil {
+		l.Fatalf("failed to run %v, envs: %v: %v, msg: %s", args, env, err, string(msg))
+	}
+}
+
+type installInfo struct {
+	Name         string
+	DownloadAddr string
+	Version      string
+}
+
+func buildInstaller(version, download string) {
+
+	data, err := ioutil.ReadFile("install.sh.template")
+	if err != nil {
+		l.Fatal(err)
+	}
+	tmp, err := template.New("install").Parse(string(data))
+	if err != nil {
+		l.Fatal(err)
+	}
+	buf := bytes.NewBufferString("")
+	err = tmp.Execute(buf, map[string]string{
+		"Version":     version,
+		"DownloadUrl": download,
+	})
+	if err != nil {
+		l.Fatal(err)
+	}
+	err = ioutil.WriteFile("install.sh", buf.Bytes(), 0664)
+	if err != nil {
+		l.Fatal(err)
+	}
+}
---
+++ cmd/make/build/pub.go
@@ -0,0 +1,222 @@
+package build
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils"
+	"gitlab.jiagouyun.com/cloudcare-tools/sec-checker/git"
+
+	"github.com/dustin/go-humanize"
+)
+
+var (
+	installerExe string
+)
+
+type versionDesc struct {
+	Version  string `json:"version"`
+	Date     string `json:"date_utc"`
+	Uploader string `json:"uploader"`
+	Branch   string `json:"branch"`
+	Commit   string `json:"commit"`
+	Go       string `json:"go"`
+}
+
+func (vd *versionDesc) withoutGitCommit() string {
+	parts := strings.Split(vd.Version, "-")
+	if len(parts) != 3 {
+		l.Fatalf("version info not in v<x.x>-<n>-g<commit-id> format: %s", vd.Version)
+	}
+
+	return strings.Join(parts[:2], "-")
+}
+
+func tarFiles(goos, goarch string) {
+
+	gz := filepath.Join(PubDir, Release, fmt.Sprintf("%s-%s-%s-%s.tar.gz",
+		AppName, goos, goarch, git.Version))
+	args := []string{
+		`czf`,
+		gz,
+		`autostart`,
+		`-C`,
+		filepath.Join(BuildDir, fmt.Sprintf("%s-%s-%s", AppName, goos, goarch)), AppBin,
+	}
+
+	cmd := exec.Command("tar", args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		l.Fatal(err)
+	}
+}
+
+func getCurrentVersionInfo(url string) *versionDesc {
+
+	l.Infof("get current online version: %s", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		l.Warn("get current online version failed, ignored")
+		return nil
+	}
+
+	defer resp.Body.Close()
+	info, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	l.Infof("current online version: %s", string(info))
+	var vd versionDesc
+	if err := json.Unmarshal(info, &vd); err != nil {
+		l.Fatal(err)
+	}
+	return &vd
+}
+
+func PubDatakit() {
+	start := time.Now()
+	var ak, sk, bucket, ossHost string
+
+	// 在你本地设置好这些 oss-key 环境变量
+	switch Release {
+	case `test`, `local`, `release`, `preprod`:
+		tag := strings.ToUpper(Release)
+		ak = os.Getenv(tag + "_OSS_ACCESS_KEY")
+		sk = os.Getenv(tag + "_OSS_SECRET_KEY")
+		bucket = os.Getenv(tag + "_OSS_BUCKET")
+		ossHost = os.Getenv(tag + "_OSS_HOST")
+	default:
+		l.Fatalf("unknown release type: %s", Release)
+	}
+
+	if ak == "" || sk == "" {
+		l.Fatalf("oss access key or secret key missing, tag=%s", strings.ToUpper(Release))
+	}
+
+	oc := &cliutils.OssCli{
+		Host:       ossHost,
+		PartSize:   512 * 1024 * 1024,
+		AccessKey:  ak,
+		SecretKey:  sk,
+		BucketName: bucket,
+		WorkDir:    OSSPath,
+	}
+
+	if err := oc.Init(); err != nil {
+		l.Fatal(err)
+	}
+
+	// 请求线上版本信息
+	url := fmt.Sprintf("http://%s.%s/%s/%s", bucket, ossHost, OSSPath, "version")
+	curVd := getCurrentVersionInfo(url)
+	_ = curVd
+
+	// upload all build archs
+	archs := []string{}
+
+	switch Archs {
+	case "all":
+		archs = OSArches
+		// read cmd-line env
+		if x := os.Getenv("ALL_ARCHS"); x != "" {
+			archs = strings.Split(x, "|")
+		}
+	case "local":
+		archs = []string{runtime.GOOS + "/" + runtime.GOARCH}
+		if x := os.Getenv("LOCAL"); x != "" {
+			archs = strings.Split(x, "|")
+		}
+	default:
+		archs = strings.Split(Archs, "|")
+	}
+
+	ossfiles := map[string]string{
+		path.Join(PubDir, Release, "version"): path.Join(OSSPath, "version"),
+	}
+
+	if Archs == "darwin/amd64" {
+		delete(ossfiles, path.Join(PubDir, Release, "version"))
+	}
+
+	renameOssFiles := map[string]string{}
+
+	// tar files and collect OSS upload/backup info
+	for _, arch := range archs {
+		if arch == "darwin/amd64" && runtime.GOOS != "darwin" {
+			l.Warn("Not a darwin system, skip the upload of related files.")
+			continue
+		}
+		parts := strings.Split(arch, "/")
+		if len(parts) != 2 {
+			l.Fatalf("invalid arch %q", parts)
+		}
+		goos, goarch := parts[0], parts[1]
+
+		tarFiles(parts[0], parts[1])
+
+		gzName := fmt.Sprintf("%s-%s-%s.tar.gz", AppName, goos+"-"+goarch, git.Version)
+
+		gzNameNoVer := fmt.Sprintf("%s-%s.tar.gz", AppName, goos+"-"+goarch)
+
+		ossfiles[path.Join(PubDir, Release, gzName)] = path.Join(OSSPath, gzName)
+
+		renameOssFiles[path.Join(OSSPath, gzName)] = path.Join(OSSPath, gzNameNoVer)
+
+		if goos == "windows" {
+			installerExe = fmt.Sprintf("installer-%s-%s.exe", goos, goarch)
+
+			// if curVd != nil && curVd.Version != git.Version {
+			// 	renameOssFiles[path.Join(OSSPath, installerExe)] =
+			// 		path.Join(OSSPath, fmt.Sprintf("installer-%s-%s-%s.exe", goos, goarch, curVd.Version))
+			// }
+
+		} else {
+			installerExe = "install.sh"
+
+			// if curVd != nil && curVd.Version != git.Version {
+			// 	renameOssFiles[path.Join(OSSPath, installerExe)] =
+			// 		path.Join(OSSPath, fmt.Sprintf("installer-%s-%s-%s", goos, goarch, curVd.Version))
+			// }
+		}
+
+		ossfiles["install.sh"] = path.Join(OSSPath, installerExe)
+	}
+
+	// test if all file ok before uploading
+	for k, _ := range ossfiles {
+		if _, err := os.Stat(k); err != nil {
+			l.Fatal(err)
+		}
+	}
+
+	for k, v := range ossfiles {
+
+		fi, _ := os.Stat(k)
+		l.Debugf("upload %s(%s)...", k, humanize.Bytes(uint64(fi.Size())))
+
+		if err := oc.Upload(k, v); err != nil {
+			l.Fatal(err)
+		}
+	}
+
+	l.Infof("Done!(elapsed: %v)", time.Since(start))
+}
