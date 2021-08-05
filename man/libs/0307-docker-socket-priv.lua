d6e4b9ee7c6b8763c9c66f1036e75ef150cd51bc
liwenjin <liwenjin@jiagouyun.com>
2021-08-05 19:38:08 +0800

    version 添加

---
+++ man/libs/0307-docker-socket-priv.lua
@@ -0,0 +1,17 @@
+local filemonitor = require("filemonitor")
+
+local function check(file)
+    local is_install_docker = get_global_cache('install_docker')
+    if is_install_docker == nil or not is_install_docker then
+        return
+    end
+    if not file_exist(file) then
+        return
+    end
+    -- 000 110 100 100   420
+    -- --- rw- r-- r--
+    filemonitor.priv_limit(file, 420)
+end
+file = '/usr/lib/systemd/system/docker.socket'
+
+check(file)
